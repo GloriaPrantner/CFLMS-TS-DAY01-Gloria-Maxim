d903cc1cc796c876d277597387033cd6883d32cf
Gloria Prantner <gloriaprantner@gmail.com>
2020-06-17 14:12:38 +0200

    finished basic exercises

---
+++ Basic/Basic 3/basic-3.js
@@ -0,0 +1,15 @@
+var array = [
+    "Peter",
+    "Johann",
+    "Paul",
+    "Julia",
+    "Lisa",
+    "Maria"
+];
+for (var i in array) {
+    document.write(i + " ");
+    document.write(array[i] + " ");
+}
+// for(let i of array){
+// 	document.write(i + " ")
+// }
---
+++ Basic/Basic 3/basic-3.ts
@@ -0,0 +1,16 @@
+let array: string[] = [
+	"Peter",
+	"Johann",
+	"Paul",
+	"Julia",
+	"Lisa",
+	"Maria"];
+
+for(let i in array){
+	document.write(i + " ")
+	document.write(array[i] + " ")
+}
+
+// for(let i of array){
+// 	document.write(i + " ")
+// }
